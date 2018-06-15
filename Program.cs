b864950a6ccc7963a326759325e46e875005e959
Lee Borlace <lee.borlace@gmail.com>
2018-06-15 14:29:16 +0800

    Initial add of files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Google.Cloud.Language.V1;
+using System;
+
+namespace GoogleNLP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The text to analyze.
+            //string text = "the dog chased the cat";
+            string text = "the man hungrily ate the delicious hamburger with a fork";
+
+            var client = LanguageServiceClient.Create();
+
+            var document = new Document()
+            {
+                Content = text,
+                Type = Document.Types.Type.PlainText
+            };
+
+            var response = client.AnalyzeSyntax(document);
+
+
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# Google Account Details
+starting-account-e0aip87xu883
+
+set GOOGLE_APPLICATION_CREDENTIALS=C:\Users\lborlace\Documents\visual studio 2017\Projects\GoogleNLP\Logic Learner-5c2494dbfe6e.json
